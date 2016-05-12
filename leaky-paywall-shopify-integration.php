b3115427aca668e1b03a801c152f0f70647a01c6
donbui <bui.don@gmail.com>
2016-05-12 01:29:27 -0400

    update

--- leaky-paywall-shopify-integration.php
+++ leaky-paywall-shopify-integration.php
@@ -10,7 +10,6 @@
 
 add_action( 'shopify_hook', 'shopify' );
 function shopify($import_all = false) {
-
 	$shop = get_option('shopify_url');
 	$key = get_option('shopify_api_key');
 	$password = get_option('shopify_password');
